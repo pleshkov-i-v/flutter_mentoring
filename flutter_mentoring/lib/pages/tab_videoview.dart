bc945c85fa8906497cd8bf07c6bc1f853e75255b
Ivan Pleshkov <Ivan_Pleshkov@epam.com>
2020-04-07 22:31:16 +0300

    video

--- flutter_mentoring/lib/pages/tab_videoview.dart
+++ flutter_mentoring/lib/pages/tab_videoview.dart
@@ -1,8 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class TabVideoView extends StatelessWidget{
+class TabVideoView extends StatefulWidget {
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<TabVideoView> {
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    // Create an store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Video"),);
+    return Column(
+      children: <Widget>[
+        FutureBuilder(
+          future: _initializeVideoPlayerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If the VideoPlayerController has finished initialization, use
+              // the data it provides to limit the aspect ratio of the VideoPlayer.
+              return AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                // Use the VideoPlayer widget to display the video.
+                child: VideoPlayer(_controller),
+              );
+            } else {
+              // If the VideoPlayerController is still initializing, show a
+              // loading spinner.
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        MaterialButton(
+          child: Text(
+            _controller.value.isPlaying ? "Pause" : "Play",
+            style: Theme.of(context).accentTextTheme.button,
+          ),
+          color: Theme.of(context).accentColor,
+          onPressed: () {
+            // Wrap the play or pause in a call to `setState`. This ensures the
+            // correct icon is shown.
+            setState(() {
+              // If the video is playing, pause it.
+              if (_controller.value.isPlaying) {
+                _controller.pause();
+              } else {
+                // If the video is paused, play it.
+                _controller.play();
+              }
+            });
+          },
+        )
+      ],
+    );
   }
 }
