263c92aa01613fe0f2f3a054feb73e844a3c3803
Ivan Pleshkov <Ivan_Pleshkov@epam.com>
2020-04-07 20:34:50 +0300

    map view

--- flutter_mentoring/lib/pages/tab_mapview.dart
+++ flutter_mentoring/lib/pages/tab_mapview.dart
@@ -1,8 +1,34 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class TabMapView extends StatelessWidget{
+class TabMapView extends StatefulWidget {
+  @override
+  TabMapViewState createState() {
+    return TabMapViewState();
+  }
+}
+
+class TabMapViewState extends State<TabMapView> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Map"),);
+    return GoogleMap(
+        mapType: MapType.hybrid,
+        myLocationButtonEnabled: false,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(53.942348, 27.566848),
+          zoom: 15.0,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        markers: (<Marker>[
+          Marker(
+              markerId: MarkerId('Work'),
+              draggable: false,
+              position: LatLng(53.942348, 27.566848))
+        ]).toSet());
   }
 }
